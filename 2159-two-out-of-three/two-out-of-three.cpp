5da11deebdc57044d3550227b5003bed0fafcb16
Sivanesh100 <Sivanesh3836@gmail.com>
2025-05-11 21:40:42 +0530

    Time: 11 ms (34.69%) | Memory: 32.8 MB (34.24%) - LeetSync

---
+++ 2159-two-out-of-three/two-out-of-three.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
+    unordered_map<int, int> count;
+    unordered_set<int> set1(nums1.begin(), nums1.end());
+    unordered_set<int> set2(nums2.begin(), nums2.end());
+    unordered_set<int> set3(nums3.begin(), nums3.end());
+
+    for (int num : set1) count[num]++;
+    for (int num : set2) count[num]++;
+    for (int num : set3) count[num]++;
+
+    vector<int> result;
+    for (auto& entry : count) {
+        if (entry.second >= 2) {
+            result.push_back(entry.first);
+        }
+    }
+
+    return result;
+    }
+};
