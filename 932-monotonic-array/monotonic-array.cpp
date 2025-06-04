7367895616e26374a750ede7f114d92cd548d104
Sivanesh100 <Sivanesh3836@gmail.com>
2025-06-04 09:49:41 +0530

    Time: 0 ms (100.00%) | Memory: 100.5 MB (30.75%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int inc=1,dec=1;
+        for (int i=1;i<nums.size();i++)
+         {
+            if(nums[i]>nums[i-1])dec=0;
+            if(nums[i]<nums[i-1])inc=0;
+        }
+        return inc || dec;
+    }
+};
